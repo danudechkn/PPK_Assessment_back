04f23be72ee51835ec3e058721bde33b09eda5f8
unknown <ppk@ICT-DEV-NES.Leelawadee.go.th>
2026-05-26 08:12:52 +0700

    first version

---
+++ src/routes/user.route.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+
+import { AllInOneController } from "../controllers/all_in_one/allInOne.controller";
+const router = Router();
+// const apiLogger = require("../middleware/apiLogger");
+// const {
+//   authenticateToken,
+//   authorizeRole,
+// } = require("../middleware/authMiddleware");
+
+//route
+// router.use(authenticateToken, apiLogger, authorizeRole(1));
+
+// router.get("/mapAll", AllChoiceController.mapAll);
+router.get("/all-in-one", AllInOneController.index);
+
+export default router;
